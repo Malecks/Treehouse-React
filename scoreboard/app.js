bd6e5549054bb4cc78e5d0a9e22db03b5ac761f4
Malecks <alex@alexmathers.com>
2021-02-26 15:28:49 -0800

    Finish part 1, course 1

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -1,20 +1,37 @@
-const title = React.createElement(
+//JSX Syntax. Requires compiling with Babel to produce React Elements
+const jsxTitle = <h1>My First React element!</h1>
+const jsxDesc = <p>I just learned how to create a React node and render it into the DOM.</p>
+
+const titleId = 'main-title';
+const desc = 'Donec ullamcorper nulla non metus auctor fringilla.';
+const name = 'Alex';
+
+const header = (
+    <header>
+        <h1 id={titleId}>{ name }'s First React Element</h1>
+        <p>{ desc }</p>
+    </header>
+)
+
+
+//React Syntax
+const reactTitle = React.createElement(
     'h1',
     { id: 'main-title', title: 'This is a title.'},
     'My First React Element!'
 );
 
-const desc = React.createElement(
+const reactDesc = React.createElement(
     'p',
     null,
     'I just learned how to create a React node and render it into the DOM.'
 );
 
-const header = React.createElement(
+const reactHeader = React.createElement(
     'header',
     null,
-    title, 
-    desc
+    reactTitle, 
+    reactDesc
 );
 
 ReactDOM.render(
