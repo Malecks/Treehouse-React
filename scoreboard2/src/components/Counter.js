72d5f198a2435507daefa072dbaefdf1f4644475
Malecks <alex@alexmathers.com>
2021-03-19 17:13:39 -0700

    Validate prop types

--- scoreboard2/src/components/Counter.js
+++ scoreboard2/src/components/Counter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Counter = ({ index, score, changeScore }) => {
     return (
@@ -9,5 +10,9 @@ const Counter = ({ index, score, changeScore }) => {
         </div>
     );
   }
+
+Counter.PropTypes = {
+
+}
 
   export default Counter;
